ced4a9e7cded85421330aa4500c5d13d8bc1f394
Simon Rösch <roesch@ubique.ch>
2017-08-15 16:00:46 +0200

    alle felder für einsätze speichern, speichern auch für zivis ermöglichen

--- web-client/src/views/tags/Profile/Missions.js
+++ web-client/src/views/tags/Profile/Missions.js
@@ -53,7 +53,7 @@ export default class Missions extends Component {
                             <div class="form-group">
                                 <label class="control-label col-sm-3" for="newmission_mission_type">Einsatzart</label>
                                 <div class="col-sm-9">
-                                    <select id="newmission_mission_type" name="newmission_mission_type" class="form-control" onChange={(e)=>self.handleSelectChange(e)}>
+                                    <select value={""+self.state['result'][missionKey+'_mission_type']} id={missionKey+'_mission_type'} name={missionKey+'_mission_type'}  class="form-control" onChange={(e)=>self.handleSelectChange(e)}>
                                         <option value="0"></option>
                                         <option value="1">Erster Einsatz</option>
                                         <option value="2">Letzter Einsatz</option>
@@ -71,11 +71,11 @@ export default class Missions extends Component {
                             <hr/>
                             <h4>Schnuppertag</h4>
                             <p>Tragen Sie nachfolgend ein, ob Sie bei der SWO einen Schnuppertag geleistet haben. Dieser wird dem Einsatz allenfalls angerechnet.</p>
-                            <DatePicker value={self.state[missionKey+'_trial_mission_date']} id={missionKey+'_trial_mission_date'} callback={self.handleDateChange} label="Datum" callbackOrigin={self} />
+                            <DatePicker value={self.state['result'][missionKey+'_probation_day']} id={missionKey+'_probation_day'} callback={self.handleDateChange} label="Datum" callbackOrigin={self} />
                             <div class="form-group">
-                                <label class="control-label col-sm-3" for={missionKey+'_trial_mission_comment'}>Bemerkungen zum Schnuppertag</label>
+                                <label class="control-label col-sm-3" for={missionKey+'_probation_day_comment'}>Bemerkungen zum Schnuppertag</label>
                                 <div class="col-sm-9">
-                                    <textarea rows="4" id={missionKey+'_trial_mission_comment'} name={missionKey+'_trial_mission_comment'} class="form-control" onChange={(e)=>self.handleTextareaChange(e)} >{self.state.result[missionKey+'_trial_mission_comment']}</textarea>
+                                    <textarea rows="4" id={missionKey+'_probation_day_comment'} name={missionKey+'_probation_day_comment'} class="form-control" onChange={(e)=>self.handleTextareaChange(e)} value={self.state.result[missionKey+'_probation_day_comment']} />
                                 </div>
                             </div>
 
@@ -84,7 +84,7 @@ export default class Missions extends Component {
                             {mission == null || mission.draft==null ? 'Provisorisch' : 'Aufgeboten, Aufgebot erhalten am '+mission.draft}
                             <hr/>
 
-                            <button class="btn btn-primary" type="submit">Daten speichern</button>
+                            {mission == null || mission.draft==null ? <button class="btn btn-primary" type="submit">Daten speichern</button> : null}
                             &nbsp;
                             {aufgebotErhaltenButton}
 
@@ -163,11 +163,14 @@ export default class Missions extends Component {
         var newMission = {
             user: self.state.result.id,
             specification: self.state.result[missionKey+'_specification'],
+            mission_type: self.state.result[missionKey+'_mission_type'],
             start: self.state.result[missionKey+'_start'],
             end: self.state.result[missionKey+'_end'],
             first_time: self.state.result[missionKey+'_first_time'],
             long_mission: self.state.result[missionKey+'_long_mission'],
-            probation_period: self.state.result[missionKey+'_probation_period']
+            probation_period: self.state.result[missionKey+'_probation_period'],
+            probation_day: self.state.result[missionKey+'_probation_day'],
+            probation_day_comment: self.state.result[missionKey+'_probation_day_comment']
         };
 
         self.setState({loading:true, error:null});
@@ -181,7 +184,7 @@ export default class Missions extends Component {
                 $("[data-dismiss=modal]").trigger({ type: "click" });
                 self.getUser();
             }).catch((error) => {
-                Toast.showError('Speichern fehlgeschlagen', 'Neuer Einsatz konnte nicht gespeichert werden', error, this.context)
+                Toast.showError('Speichern fehlgeschlagen', 'Neuer Einsatz konnte nicht gespeichert werden', error, self.context)
             });
         }else{
             axios.post(
@@ -193,7 +196,7 @@ export default class Missions extends Component {
                 $("[data-dismiss=modal]").trigger({ type: "click" });
                 self.getUser();
             }).catch((error) => {
-                Toast.showError('Speichern fehlgeschlagen', 'Einsatz konnte nicht gespeichert werden', error, this.context)
+                Toast.showError('Speichern fehlgeschlagen', 'Einsatz konnte nicht gespeichert werden', error, self.context)
             });
         }
     }
@@ -208,7 +211,7 @@ export default class Missions extends Component {
             Toast.showSuccess('Löschen erfolgreich', 'Einsatz konnte gelöscht werden')
             self.getUser();
         }).catch((error) => {
-            Toast.showError('Löschen fehlgeschlagen', 'Einsatz konnte nicht gelöscht werden', error, this.context)
+            Toast.showError('Löschen fehlgeschlagen', 'Einsatz konnte nicht gelöscht werden', error, self.context)
         });
 
     }
