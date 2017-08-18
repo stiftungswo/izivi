405e417836a63653005af5343cca987fea3022e0
Michael Schaer [Notebook] <mdschaer@gmail.com>
2017-08-18 16:21:15 +0200

    User Feedbacks

--- web-client/src/views/pages/user_feedback_overview.js
+++ web-client/src/views/pages/user_feedback_overview.js
@@ -6,34 +6,40 @@ import Component from 'inferno-component';
 import ApiService from "../../utils/api";
 import LoadingView from "../tags/loading-view";
 import Header from "../tags/header";
-
+import DatePicker from '../tags/InputFields/DatePicker';
 
 export default class UserFeedbackOverview extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            year:new Date().getFullYear(),
             loading:false,
             error:null,
             feedbacks: [],
-            questions: []
+            questions: [],
+            date_from: moment().subtract(1, 'year'),
+            date_to: moment(),
         };
     }
 
     componentDidMount() {
         this.getFeedbackAnswers();
+        DatePicker.initializeDatePicker();
+    }
+
+    dbDate(sec) {
+        return moment(sec).format('YYYY-MM-DD HH:mm:ss');
     }
 
     getFeedbackAnswers() {
         this.setState({loading: true, error: null});
 
         axios.get(
-            ApiService.BASE_URL+'user/feedback',
-            { headers: { Authorization: "Bearer " + localStorage.getItem('jwtToken') } }
+            ApiService.BASE_URL+'user/feedback?date_from='+this.dbDate(this.state.date_from) + '&date_to='+this.dbDate(this.state.date_to),
+            { headers: { Authorization: "Bearer " + localStorage.getItem('jwtToken') }
+            }
         ).then((response) => {
             this.setState({loading: false});
-            //console.log("response feedbacks = ", response.data)
             this.setState({
                 feedbacks: response.data
             });
@@ -42,11 +48,27 @@ export default class UserFeedbackOverview extends Component {
         });
     }
 
+    handleDateChange(e, origin) {
+        let lastValue = origin.state[e.target.name];
+        let value = e.target.value;
+
+        if(value !== undefined && value != null && value != "") {
+            value = DatePicker.dateFormat_CH2EN(value);
+        }
+
+        origin.state[e.target.name] = value;
+        origin.setState(this.state);
+
+        if(!moment(value).isSame(lastValue, 'day')){
+            origin.getFeedbackAnswers();
+        }
+    }
+
     getChapter(title) {
         return (
             <div>
-                <div class="row">
-                    <div class="col-xs-12 chapter">
+                <div class="row chapter">
+                    <div class="col-xs-12">
                         <h3>
                             {title}
                         </h3>
@@ -66,7 +88,21 @@ export default class UserFeedbackOverview extends Component {
         for (var i = 1; i <= numberOfAnswers; i++) {
 
             let answerPerc = 0;
-            let answerClasses = ["", "progress-bar-danger", "progress-bar-warning", "progress-bar-info", "progress-bar-success"];
+            let answerClasses;
+
+            if(numberOfAnswers == 2) {
+                answerClasses = ["", "progress-bar-danger", "progress-bar-success"];
+
+                if(i == 2) {
+                    cols.push(<div class={"col-xs-6"}></div>);
+                }
+            }
+            else if(numberOfAnswers == 6) {
+                answerClasses = ["", "progress-bar-info", "progress-bar-info", "progress-bar-info", "progress-bar-info", "progress-bar-info", "progress-bar-info"];
+            }
+            else {
+                answerClasses = ["", "progress-bar-danger", "progress-bar-warning", "progress-bar-info", "progress-bar-success"];
+            }
 
             if(answers[i] == 0) {
                 answerPerc = 0;
@@ -131,7 +167,6 @@ export default class UserFeedbackOverview extends Component {
                 for (var i = 1; i <= 6; i++) {
                     answersCleaned[i] = answers[x]['answers'][i] ? answers[x]['answers'][i] : 0;
                     totalAnswers += answersCleaned[i];
-                    console.log(totalAnswers);
                 }
 
                 // Question types
@@ -182,7 +217,7 @@ export default class UserFeedbackOverview extends Component {
                             </div>
                             <div class="col-xs-2">
                                 <div class="row">
-                                    { this.getType1RowContent(2, answersCleaned, totalAnswers, 4) }
+                                    { this.getType1RowContent(2, answersCleaned, totalAnswers) }
                                 </div>
                             </div>
                             <div class="col-xs-1">
@@ -209,22 +244,29 @@ export default class UserFeedbackOverview extends Component {
                             </div>
                         </div>);
                 }
-
             }
             else if (answers[x].type == 0) {
                 feedbacks.push(
+                    <div>
+                    <br /><br />
                     <div class="row">
                         <div class="col-xs-8">
-                            <label>
-                                <h4>{answers[x].question}</h4>
-                            </label>
+                            <label><h4>{answers[x].question}</h4></label>
                         </div>
-                        <div class="col-xs-4">
+                        <div class="col-xs-1">
+                            <label>{answers[x].opt1}</label>
                         </div>
-                    </div>)
+                        <div class="col-xs-2"></div>
+                        <div class="col-xs-1">
+                            <label>{answers[x].opt2}</label>
+                        </div>
+                    </div>
+                    </div>
+                )
             }
             else if (answers[x].type == 2) {
                 feedbacks.push(
+                    <div>
                     <div class="row">
                         <div class="col-xs-6">
                             <label>
@@ -235,6 +277,8 @@ export default class UserFeedbackOverview extends Component {
                             <textarea class="form-control" rows="10" cols="50"
                                       value={answers[x] ? answers[x]['answers'] : null} name={x}/>
                         </div>
+                    </div>
+                    <br /><br />
                     </div>)
             }
         }
@@ -243,20 +287,25 @@ export default class UserFeedbackOverview extends Component {
             <Header>
                 <div className="page page__user_feedback_overview">
                     <Card>
-                        <div class="container title">
+                        <div class="container top">
                             <div class="row">
-                                <div class="col-xs-12">
-                                    <h4>
-                                        Anzahl Feedbacks in diesem Jahr: {totalFeedbacks}
-                                    </h4>
+                                <div class="col-sm-4">
+                                    <DatePicker id="date_from" label="Von" value={this.state.date_from} callback={this.handleDateChange} callbackOrigin={this} />
+                                </div>
+                                <div class="col-sm-4">
+                                    <DatePicker id="date_to" label="Bis" value={this.state.date_to} callback={this.handleDateChange} callbackOrigin={this} />
+                                </div>
+                                <div class="col-sm-4">
+                                    <h5>Anzahl Feedbacks: {totalFeedbacks}</h5>
                                 </div>
                             </div>
                         </div>
                         <div class="container feedback">
-                        {feedbacks}
+                            <div class="innerContainer">
+                            {feedbacks}
+                            </div>
                         </div>
                     </Card>
-
                     <LoadingView loading={this.state.loading} error={this.state.error}/>
                 </div>
             </Header>
