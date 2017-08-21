02c70ad0a038d6ffec8004f584b1faef0b040960
Clot Schaniel <clot@schaniel.net>
2017-08-21 14:19:58 +0200

    added button for giving feedback on profile. Todo set feedback_done attribute on Missions table to 1 if done.

--- web-client/src/views/tags/Profile/Missions.js
+++ web-client/src/views/tags/Profile/Missions.js
@@ -140,13 +140,21 @@ export default class Missions extends Component {
                     </button>)
                 }
 
+                var disabledMission = []
+                if(m[i].end >= new Date() || m[i].feedback_done == 1){
+                    disabledMission.push(<div class="col-xs-1"><a href="/user_feedback"><button class="btn btn-xs btn-info" disabled> Feedback</button></a></div>)
+                } else {
+                    disabledMission.push(<div class="col-xs-1"><a href="/user_feedback"><button class="btn btn-xs btn-info"> Feedback</button></a></div>)
+                }
+
                 missions.push(
                     <div class="row">
                         <div class="col-xs-2">{name}</div>
                         <div class="col-xs-2">{moment(m[i].start, 'YYYY-MM-DD').format('DD.MM.YYYY')}</div>
-                        <div class="col-xs-3">{moment(m[i].end, 'YYYY-MM-DD').format('DD.MM.YYYY')}</div>
+                        <div class="col-xs-2">{moment(m[i].end, 'YYYY-MM-DD').format('DD.MM.YYYY')}</div>
                         <div class="col-xs-1"><a class="btn btn-xs"  href={ApiService.BASE_URL+'mission/'+curMission.id+'/draft?jwttoken='+encodeURI(localStorage.getItem('jwtToken'))} target="_blank"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Drucken</a></div>
                         <div class="col-xs-1"><button class="btn btn-xs btn-warning" data-toggle="modal" data-target={'#einsatzModal'+m[i].id}><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Bearbeiten</button></div>
+                        {disabledMission}
                         <div class="col-xs-1">{deleteButton}</div>
                         <div class="col-xs-1">{addButton}</div>
                     </div>
